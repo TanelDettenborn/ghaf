91f9822f553763a59385d2f79d1b02bd014d1dff
Tanel Dettenborn <tanel@ssrc.tii.ae>
2024-12-10 13:17:38 +0200

    Nvidia Orin NX/AGX: Using device trees from BSP (not from upstream linux)
    Signed-off-by: Tanel Dettenborn <tanel@ssrc.tii.ae>

--- modules/reference/hardware/jetpack/nvidia-jetson-orin/jetson-orin.nix
+++ modules/reference/hardware/jetpack/nvidia-jetson-orin/jetson-orin.nix
@@ -113,10 +113,12 @@ in
           "${lib.getDev config.hardware.deviceTree.kernelPackage}/lib/modules/${config.hardware.deviceTree.kernelPackage.modDirVersion}/source/nvidia/soc/t23x/kernel-include"
         ];
       }
+
+      # NOTE: "-nv.dtb" files are from NVIDIA's BSP
       # Versions of the device tree without PCI passthrough related
       # modifications.
       // lib.optionalAttrs (cfg.somType == "agx") {
-        name = lib.mkDefault "tegra234-p3737-0000+p3701-0000.dtb";
+        name = lib.mkDefault "tegra234-p3737-0000+p3701-0000-nv.dtb";
       }
       // lib.optionalAttrs (cfg.somType == "nx") {
         # Sake of clarity: Jetson 35.4 and IO BASE B carrier board
@@ -127,7 +129,7 @@ in
         # Upstream kernel has only official carrier board device tree,
         # but it works with IO BASE B carrier board with minor
         # modifications.
-        name = lib.mkDefault "tegra234-p3768-0000+p3767-0000.dtb";
+        name = lib.mkDefault "tegra234-p3768-0000+p3767-0000-nv.dtb";
       };
   };
 }
